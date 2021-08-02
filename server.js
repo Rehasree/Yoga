297961a3d3d2557e297dddd637acfd3d8943a758
Nihar Sanda <nihar.sanda@gmail.com>
2021-08-02 10:23:32 +0530

    deployment changes added

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import path from 'path';
 
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
+var __dirname = path.resolve();
 
 const app = express();
 
@@ -17,10 +19,25 @@ app.use('/posts', postRoutes);
 app.use('/user',userRoutes);
 
 const CONNECTION_URL = "mongodb+srv://Munny:Munny@2002@cluster0.6d1os.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB successfully connected"))
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendfile(
+      path.join(__dirname = 'client/build/index.html')
+    );
+  }
+  )
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
